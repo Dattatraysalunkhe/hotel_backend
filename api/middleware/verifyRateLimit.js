88f4e7311c45bc73d2e592c37c564937db774d1e
Dattatray Salunkhe <149805576+Dattatraysalunkhe@users.noreply.github.com>
2025-10-25 22:33:07 +0530

    fix:limit set

--- api/middleware/verifyRateLimit.js
+++ api/middleware/verifyRateLimit.js
@@ -4,7 +4,7 @@ import rateLimit from "express-rate-limit"
 const CheckRateLimit = rateLimit({
     windowMs: 30 * 60 * 1000,  // 15 minutes
     // max:10,
-    max: 500,
+    max: 40,
     message: "Too many requests, please try again later.",
     skip: (req, res) => {
     // Skip rate limiting for OPTIONS requests (CORS preflight)
